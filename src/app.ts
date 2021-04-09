7a6ffb0587c040c5e2d57da7c375d55a26763cec
Kenny <kennyquintana297@gmail.com>
2021-04-09 00:30:33 -0400

    problem8

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,33 @@
-var cad1: string = "ksadussss";
-var cad2: string = "zdsdsa";
-var total: number = 0;
-var visitados : number [] = [];
+var num: number = 40 ;
+var resp: string = "[";
+
+primo2(num, 2);
 
 
-for(var i: number = 0; i<= cad1.length-1; i++){
-    total+=encontrar(cad1.charAt(i),0);
-    //console.log(encontrar(cad1.charAt(i),0))
+function primo2(m:number, prim:number) {
+    var i:number=0;
+    while(i<m){
+        if (verificar(prim,2)){
+            if(i!=m-1) {
+                resp+=prim+", ";
+            }
+            else { 
+                resp+=prim+"]";
+            }
+            i+=1;
+        }
+        prim+=1;       
+    }
+    console.log(resp);
 }
 
-console.log(total);
-
-
-
-
-function encontrar(caracter: string, posicioncad2: number) : number{
-    if (posicioncad2 == cad2.length)
-            return 0;
-    else{
-        if(caracter == cad2.charAt(posicioncad2)){
-            for(var i:number = 0; i< visitados.length; i++){
-                if(posicioncad2 == visitados[i]) {
-                    return encontrar(caracter,posicioncad2+1);
-                }
-            }
-            visitados.push(posicioncad2);
-            return 1;
-        }
-        return encontrar(caracter,posicioncad2+1);
+function verificar(n:number,div:number):boolean{
+    if(n/2 < div)
+        return true;
+    else {
+        if (n % div == 0){
+            return false;
+        }   
+        return verificar(n, div+1);
     }
 }
