8e7064ebd2ab79890af3f914981a7b59a49ea7c2
Kenny <kennyquintana297@gmail.com>
2021-04-09 00:25:55 -0400

    problem5

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,41 @@
-var vector :number[] = [3, 6, -2, 10, 7, 3];
-var maximo:number = 0;
+var vector: number[] = [5,9,15,1,3,20];
+var min:number = 0;
+var max:number = 0;
+console.log(minimo());
 
-console.log(producto(0));
 
-
-
-function producto(pos:number):number {
-    if(pos>=vector.length-1)
-        return maximo;
-    else{
-        if((vector[pos]*vector[pos+1])>maximo)
-            maximo=vector[pos]*vector[pos+1];
-        return producto(pos+1); 
+function minimo():number {
+    for(var i=0; i<vector.length;i++) {
+        if(i==0) {
+            max=vector[i];
+            min=vector[i];
+        }
+        else {
+            if(vector[i]>max) {
+                max = vector[i];
+            }
+            if(vector[i]<min) {
+                min = vector[i];
+            }
+        }    
     }
+    return calculos();
+}
+
+function calculos() :number{
+    let faltantes:number=0;
+    let respuesta:number=0;
+    for(var i:number=min ; i<=max ; i++){
+        for(var j:number =0; j<vector.length ; j++) {
+            if(i != vector[j]) {
+                faltantes++;
+            }
+            if(faltantes===vector.length){
+                respuesta++;
+                //console.log(i);
+            }
+        }
+        faltantes=0;
+    }
+    return respuesta;
 }
