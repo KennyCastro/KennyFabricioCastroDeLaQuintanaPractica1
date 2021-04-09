109b2aaa1c31ce2edb351c6aa4ba82df1996a80c
Kenny <kennyquintana297@gmail.com>
2021-04-09 00:34:41 -0400

    problem10

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,72 @@
-var num: number = 40 ;
-var resp: string = "[";
-
-primo2(num, 2);
+var ipv4: string = "172.168.10.10";
+var sub: string, inicio:number = 0, fin: number = 0, cont:number = 0;
 
 
-function primo2(m:number, prim:number) {
-    var i:number=0;
-    while(i<m){
-        if (verificar(prim,2)){
-            if(i!=m-1) {
-                resp+=prim+", ";
+
+verificar2();
+
+function verificar2() {
+    if(ipv4.length <=15 && ipv4.length >= 7){
+        while(inicio<ipv4.length){
+            var f:number = verpunto();
+            if(f == inicio){
+                console.log("false");
+                break;
             }
-            else { 
-                resp+=prim+"]";
+            else{
+                sub = ipv4.substring(inicio,fin);
+                inicio = f+1;
+                if(!validarentero(sub)){  
+                    break;
+                    
+                }
+                
+                cont++;
             }
-            i+=1;
         }
-        prim+=1;       
+        if(cont==4)
+            console.log("true");
+        else
+        console.log("false");
     }
-    console.log(resp);
+    else {
+        console.log("false")
+    }
+    
 }
 
-function verificar(n:number,div:number):boolean{
-    if(n/2 < div)
-        return true;
-    else {
-        if (n % div == 0){
+function validarentero(o:string): boolean{
+    var octeto:number = parseInt(o);
+    if(!isNaN(octeto)){
+        if(octeto < 0 || octeto>255)
+        {
+            
+            //console.log("false")
             return false;
-        }   
-        return verificar(n, div+1);
+        }
+        else{
+            return true;
+        }
     }
+    else
+        return false;
+}
+
+
+
+function verpunto(): number{
+    for (var i:number = inicio;i<ipv4.length;i++){
+        if(ipv4.charAt(i)=="." || i==ipv4.length-1){
+            if (i==ipv4.length-1){
+                fin=i+1;
+            }
+            else {
+                fin=i;
+            }
+            
+            break;
+        }
+    }
+    return fin;
+
 }
